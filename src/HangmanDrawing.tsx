c04c62f8769dcc7674d065cfa3b605da716890c4
Jason Gates <gatesjasond@gmail.com>
2022-11-01 18:31:11 -0500

    added all body components

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -12,9 +12,77 @@ const HEAD = (
   }}></div>
 )
 
+const BODY = (
+  <div style={{
+    width: '10px',
+    height: '100px',
+    background: 'black',
+    position: 'absolute',
+    top: '120px',
+    right: 0,
+  }}></div>
+)
+
+const RIGHT_ARM = (
+  <div style={{
+    width: '100px',
+    height: '10px',
+    background: 'black',
+    position: 'absolute',
+    top: '150px',
+    right: '-100px',
+    rotate: '-30deg',
+    transformOrigin: 'left bottom',
+  }}></div>
+)
+
+const LEFT_ARM = (
+  <div style={{
+    width: '100px',
+    height: '10px',
+    background: 'black',
+    position: 'absolute',
+    top: '150px',
+    right: '10px',
+    rotate: '30deg',
+    transformOrigin: 'right bottom',
+  }}></div>
+)
+
+const RIGHT_LEG = (
+  <div style={{
+    width: '100px',
+    height: '10px',
+    background: 'black',
+    position: 'absolute',
+    top: '210px',
+    right: '-90px',
+    rotate: '60deg',
+    transformOrigin: 'left bottom',
+  }}></div>
+)
+
+const LEFT_LEG = (
+  <div style={{
+    width: '100px',
+    height: '10px',
+    background: 'black',
+    position: 'absolute',
+    top: '210px',
+    right: '0px',
+    rotate: '-60deg',
+    transformOrigin: 'right bottom',
+  }}></div>
+)
+
 export default function HangmanDrawing() {
   return <div style={{position: 'relative'}}>
     {HEAD}
+    {BODY}
+    {RIGHT_ARM}
+    {LEFT_ARM}
+    {RIGHT_LEG}
+    {LEFT_LEG}
     {/* Hangman's Noose */}
     <div style={{
       height: '50px',
